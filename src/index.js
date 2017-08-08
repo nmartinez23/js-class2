402c7f76956bd97e653dbb298d75ea39adb5e382
Nick Martinez <23nicholasmartinez@gmail.com>
2017-08-08 09:36:03 -0700

    Add html and javascript files

---
+++ src/index.js
@@ -0,0 +1,35 @@
+function onReady () {
+  const addTodoForm = document.getElementById( 'addTodoForm' );
+  const todoList = document.getElementById( 'todoList' );
+  const newTodoText = document.getElementById( 'newTodoText' );
+
+  // <ul></ul>
+  todoList.textContent = '';
+
+  addTodoForm.addEventListener( 'submit', event => {
+    event.preventDefault(); // no DOM reload
+    const title = newTodoText.value;
+
+    // add it to the list
+    // create an li
+    // <li>{title}</li>
+    const newLi = document.createElement( 'li' );
+    newLi.textContent = title; // vs newLi.innerHTML
+
+    newLi.addEventListener( 'click', () => {
+      newLi.classList.toggle( 'todo--complete' );
+    });
+
+    // put it in the ul
+    todoList.appendChild( newLi );
+
+    newTodoText.value = '';
+  });
+}
+
+if ( document.readyState !== 'loading' ) {
+  onReady();
+} else {
+  document.addEventListener( 'DOMContentLoaded', onReady );
+}
+
