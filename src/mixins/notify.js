7a76b81ad5b51fb99cc1cc98fabd3a3d1e62ce9f
Nick Martinez <23nicholasmartinez@gmail.com>
2017-08-21 16:38:46 -0700

    Add mixin notify, setState and getState functions

---
+++ src/mixins/notify.js
@@ -0,0 +1,17 @@
+export default {
+  _subscribers: [],
+
+  subscribe ( fn ) {
+    this._subscribers.push( fn );
+
+    return () => this.unsubscribe( fn );
+  },
+
+  unsubscribe ( fn ) {
+    this._subscribers = this._subscribers.filter( f => f !== fn );
+  },
+
+  notify () {
+    this._subscribers.forEach( fn => fn() );
+  },
+};
