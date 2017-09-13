8e4c551935f1634fcdb9c0eb959c5b68647ae13e
Nick Martinez <23nicholasmartinez@gmail.com>
2017-09-13 08:55:34 -0700

    Update API with todo Promises

--- src/api.js
+++ src/api.js
@@ -18,4 +18,70 @@ export default {
     complete: false,
   },
   ],
-}
+
+  get () {
+    return new Promise( ( resolve, reject ) => {
+      resolve( this._todos );
+    });
+  },
+
+  create ( title ) {
+    return new Promise( ( resolve, reject ) => {
+      const todo = {
+        title,
+        complete: false,
+        id: shortid.generate(),
+      };
+
+      this._todos.push( todo );
+      resolve( todo );
+    });
+  },
+
+  getOne ( id ) {
+    return new Promise( ( resolve, reject ) => {
+      const todo = this._todos.find( t => t.id === id );
+
+      if ( todo ) {
+        return resolve( todo );
+      }
+
+      return reject();
+    });
+  },
+
+  update ( id, props ) {
+    return new Promise( ( resolve, reject ) => {
+      let todo;
+
+      this._todos = this._todos.map( t => {
+        if ( t.id !== id ) {
+          return t;
+        }
+
+        todo = {
+          ...t,
+          ...props,
+          id,
+        };
+
+        return todo;
+      });
+
+      resolve( todo );
+    });
+  },
+
+  remove ( id ) {
+    return new Promise( ( resolve, reject ) => {
+      this._todos = this._todos.filter( t => t.id !== id );
+
+      resolve();
+    });
+  },
+};
+
+
+
+
+
