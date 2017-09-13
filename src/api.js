7a44c8fb926e3376433f73bf651e7054a9e67197
Nick Martinez <23nicholasmartinez@gmail.com>
2017-09-13 08:25:48 -0700

    Add todo examples to test API requests

---
+++ src/api.js
@@ -0,0 +1,21 @@
+import shortid from 'shortid';
+
+export default {
+  _todos: [
+  {
+    id: '1',
+    title: 'First todo',
+    complete: false,
+  },
+  {
+    id: '2',
+    title: 'Second todo',
+    complete: true,
+  },
+  {
+    id: '3',
+    title: 'Third todo',
+    complete: false,
+  },
+  ],
+}
